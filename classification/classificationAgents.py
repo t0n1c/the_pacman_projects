4d828f9b8adef066afb4b396b6dbb69e7d63c237
antonio_caminero <antonio.caminero.garcia@gmail.com>
2016-06-27 20:03:54 +0200

    relative imports done

--- classification/classificationAgents.py
+++ classification/classificationAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -14,25 +14,21 @@
 
 # pacmanAgents.py
 # ---------------
-# Licensing Information:  You are free to use or extend these projects for 
-# educational purposes provided that (1) you do not distribute or publish 
-# solutions, (2) you retain this notice, and (3) you provide clear 
-# attribution to UC Berkeley, including a link to 
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to
 # http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero 
+# The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and 
+# Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
-from pacman import Directions
-from game import Agent
-
-import random
-import game
-import util
+from . import perceptron_pacman
+from .dataClassifier import runClassifier, enhancedFeatureExtractorPacman
+from ..game import Agent
 
 class DummyOptions:
     def __init__(self):
@@ -43,11 +39,10 @@ class DummyOptions:
         self.weights = False
 
 
-import perceptron_pacman
 
 class ClassifierAgent(Agent):
+
     def __init__(self, trainingData=None, validationData=None, classifierType="perceptron", agentToClone=None, numTraining=3):
-        from dataClassifier import runClassifier, enhancedFeatureExtractorPacman
         legalLabels = ['Stop', 'West', 'East', 'North', 'South']
         if(classifierType == "perceptron"):
             classifier = perceptron_pacman.PerceptronClassifierPacman(legalLabels,numTraining)
@@ -63,10 +58,10 @@ class ClassifierAgent(Agent):
         options = DummyOptions()
         options.classifier = classifierType
         runClassifier(args, options)
+
     def getAction(self, state):
-        from dataClassifier import runClassifier, enhancedFeatureExtractorPacman
         features = self.featureFunction(state)
-        
+
         action =  self.classifier.classify([features])[0]
 
         return action
